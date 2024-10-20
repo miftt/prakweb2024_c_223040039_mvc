f2927f903bf3f13490278972a9a1e07669188a5e
miftt <miftahfauzi012@gmail.com>
2024-10-21 00:13:09 +0700

    controller

--- phpmvc/app/core/App.php
+++ phpmvc/app/core/App.php
@@ -2,11 +2,38 @@
 
 class App
 {
-
+    //properti
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
     public function __construct()
     {
         $url = $this->parseURL();
         var_dump($url);
+        $url = $this->parseURL();
+        // controler
+        if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+            // jadi kontroler
+            $this->controller = $url[0];
+            // hilangin array ke 0
+            unset($url[0]);
+        }
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+        //method
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+        // parameter
+        if (!empty($url)) {
+            // var_dump($url);
+            $this->params = array_values($url);
+        }
+        // jalankan controller dan method serta kirim params jika ada
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseURL()
